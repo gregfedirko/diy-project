75b4d0a39ef9a48b24890d34d085951807a3957b
Greg Fedirko <gjfedirko@gmail.com>
2015-01-18 09:12:18 -0800

    add bootstrap

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+var exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+var port = 3000;
+
+app.listen(port);
+console.log('listening on port...' + port);
