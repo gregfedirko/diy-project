4c483dc915f3bbc2376e3b6ed11e109a6ac58242
Greg Fedirko <gjfedirko@gmail.com>
2015-01-18 10:37:27 -0800

    render featured from api

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request = require('request');
 
 var app = express();
 
@@ -8,8 +9,26 @@ var exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var diyConfig = {
+  url: 'http://api.diy.org'
+};
+
 app.get('/', function(req, res) {
-  res.render('index');
+
+  var requestMap = {
+    baseUrl: diyConfig.url,
+    apiRoute: '/explore/featured',
+    size: 5,
+    offset: 0
+  };
+
+  paginatedRequest(requestMap, function(body) {
+    var data = {
+      featured: body.response
+    };
+    res.render('index', data);
+  });
+
 });
 
 app.get('/about', function(req, res) {
@@ -19,9 +38,27 @@ app.get('/about', function(req, res) {
 app.get('*', function(req, res) {
   res.status(404);
   res.render('404');
-})
+});
 
 var port = 3000;
 
 app.listen(port);
 console.log('listening on port...' + port);
+
+
+function paginatedRequest(optionsMap, callback) {
+
+  var size = optionsMap.size || 5;
+  var offset = optionsMap.offset || 0;
+
+  var url = optionsMap.baseUrl.concat(optionsMap.apiRoute, '?', 'limit=', size, '&', 'offset=', offset);
+
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var parsedBody = JSON.parse(body);
+      callback(parsedBody);
+    }
+  });
+
+}
+
